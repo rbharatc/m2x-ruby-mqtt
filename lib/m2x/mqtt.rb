159ab5b2f83c27093098dd1df51981a3e5efc9c5
Federico Saravia <fedesaravia@gmail.com>
2015-02-24 18:31:02 -0300

    Reorganize file structure

--- lib/m2x/mqtt.rb
+++ lib/m2x/mqtt.rb
@@ -1,38 +1,47 @@
-class M2X::MQTT
-  attr_accessor :client
+module M2X
+  class MQTT
+    require_relative "mqtt/version"
+    require_relative "mqtt/client"
+    require_relative "mqtt/resource"
+    require_relative "mqtt/device"
+    require_relative "mqtt/distribution"
+    require_relative "mqtt/stream"
 
-  def initialize(api_key, options={})
-    @api_key = api_key
-    @options = options
-  end
+    attr_accessor :client
 
-  def client
-    @client ||= M2X::MQTT::Client.new(@api_key, @options)
-  end
+    def initialize(api_key, options={})
+      @api_key = api_key
+      @options = options
+    end
 
-  # Returns the status of the M2X system.
-  #
-  # The response to this endpoint is an object in which each of its attributes
-  # represents an M2X subsystem and its current status.
-  def status
-    client.subscribe
-    client.get("/status")
-    client.get_response
-  end
+    def client
+      @client ||= M2X::MQTT::Client.new(@api_key, @options)
+    end
 
-  def device(id)
-    M2X::MQTT::Device.new(client, "id" => id)
-  end
+    # Returns the status of the M2X system.
+    #
+    # The response to this endpoint is an object in which each of its attributes
+    # represents an M2X subsystem and its current status.
+    def status
+      client.subscribe
+      client.get("/status")
+      client.get_response
+    end
 
-  def create_device(attributes)
-    M2X::MQTT::Device.create!(client, attributes)
-  end
+    def device(id)
+      M2X::MQTT::Device.new(client, "id" => id)
+    end
 
-  def distribution(id)
-    M2X::MQTT::Distribution.new(client, "id" => id)
-  end
+    def create_device(attributes)
+      M2X::MQTT::Device.create!(client, attributes)
+    end
 
-  def stream(device_id, name)
-    M2X::MQTT::Stream.new(client, device(device_id), "name" => name)
+    def distribution(id)
+      M2X::MQTT::Distribution.new(client, "id" => id)
+    end
+
+    def stream(device_id, name)
+      M2X::MQTT::Stream.new(client, device(device_id), "name" => name)
+    end
   end
 end
