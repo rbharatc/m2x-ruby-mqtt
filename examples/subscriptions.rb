a4dd5f968d26686c40a27a30f9738dd00c5052b5
Federico Saravia <fedesaravia@gmail.com>
2015-02-25 20:27:57 -0300

    Add subscription example

---
+++ examples/subscriptions.rb
@@ -0,0 +1,32 @@
+#! /usr/bin/env ruby
+require "m2x/mqtt"
+
+API_KEY = ENV.fetch("API_KEY")
+DEVICE  = ENV.fetch("DEVICE")
+
+$m2x = M2X::MQTT.new(API_KEY)
+
+@run = true
+
+stop = Proc.new{ @run = false }
+
+trap(:INT, &stop)
+
+pid = fork do
+        $m2x.client.get_response do |payload|
+          puts payload
+        end
+      end
+
+device = $m2x.device(DEVICE)
+stream = device.stream("temperature")
+
+stream.update! # Create the stream if it doesn't exist
+
+while @run
+  stream.update_value rand(20..100)
+
+  sleep 1
+end
+
+Process.kill(:TERM, pid)
